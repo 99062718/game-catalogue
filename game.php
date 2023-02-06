53631b4ea8aa618877184f631797504ca8d89122
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2023-02-06 23:17:11 +0100

    added text to game.php
    this text includes: description, genres, number of players, developer, publisher and date of release

--- game.php
+++ game.php
@@ -8,11 +8,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/game.css">
     <title><?php echo $gameData["name"] ?></title>
 </head>
 <body>
-    <div>
+    <div id="contentDiv">
         <img src="img/<?php echo $_GET["id"] ?>/cover.png" alt="cover">
+        <p>
+            Description: <?php echo $gameData["description"] ?><br>
+            Genre(s): <?php echo $gameData["genre"] ?><br>
+            # of players: <?php echo $gameData["multi"] ?><br>
+            Developer: <?php echo $gameData["developer"] ?><br>
+            Publisher: <?php echo $gameData["publisher"] ?><br>
+            Date of release: <?php echo $gameData["date"] ?>
+        </p>
     </div>
 </body>
 </html>
