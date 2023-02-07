da12917de0b583c9f007354a68d4fea11161e0c6
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2023-02-07 14:26:55 +0100

    added navbar styling to all pages

--- game.php
+++ game.php
@@ -10,6 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/navbar.css">
     <link rel="stylesheet" href="style/game.css">
     <title><?php echo $gameData["name"] ?></title>
 </head>
